bd612343d8299fc8515769cf87507265dfd14c44
Laukin <1004320638@qq.com>
2019-11-05 19:22:40 +0800

    ....

---
+++ day2/react-project/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+export default class Header extends React.Component {
+    // eslint-disable-next-line no-useless-constructor
+    constructor(props) {
+        super(props)
+        
+    }
+    render() {
+        return (
+            <header>微信</header>
+        )
+    }
+}
